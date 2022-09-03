8b025a9beeed152b1ef3f8434f71052ceebdcccc
BerylOdoyo <berylkimberly@gmail.com>
2022-09-03 23:34:56 +0300

    First push to github

---
+++ madlib.py
@@ -0,0 +1,21 @@
+# an example of a long madlbgame
+noun1 = input("Noun: ")
+adj1 = input("Adjective:")
+verb = input("Verb: ")
+noun2 = input("Second Noun: ")
+noun3 = input("Third Noun: ")
+celeb = input("Celebrity: ")
+adj2 = input("Second Adjective: ")
+color1 = input("Color: ")
+color2 = input("Second Color: ")
+food = input("Food: ")
+
+madlib = f"Once upon a time in a far away {noun1} there lived a {adj1} princess who loved {verb}. T'was lovely and\
+ peaceful till an attack happened and the King had to hide his {noun2} in a far away land. The princess quickly\
+ adapted to the changes that had occurred and found solace in the {noun3} around her. Then one day she met {celeb}\
+ and her sad days became {adj2} again. They traveled the vast land and suddenly they met a sobbing lion who\
+ gave them {color1} and {color2} glass slippers. They had to tap the glass slippers twice\
+ to go to a magical land and they did so. All of a sudden they found themselves in in in in.......ha ha ha ha the\
+ writer is too lazy to complete this story and is a {food}"
+
+print(madlib)
